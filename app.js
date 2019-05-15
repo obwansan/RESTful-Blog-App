689243f8a3a19dc059e58a089a38e17e1a120086
Kevin O'Brien <kob123@hotmail.co.uk>
2019-05-15 22:26:26 +0100

    Blog app layout and start of NEW route

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.static("public"));
 // Have to configure this to use req.body.whatever
 app.use(bodyParser.urlencoded({extended:true}));
 
-// Create a blog schema (pattern of data every blog must have, bit like a class interface in PHP and ES6)
+// Create a blog schema (pattern of data every blog must have, bit like a class interface in PHP and Typescript)
 var blogSchema = new mongoose.Schema({
   title: String,
   image: String,
@@ -24,11 +24,7 @@ var blogSchema = new mongoose.Schema({
   created: {type: Date, default: Date.now}
 });
 
-// Compile the Blog schema into a Blog model (kind of like a class in PHP) that can 
-// be used to create, read, update and delete blogs in the DB.
-// You pass the singular version of your model (called "Blog") and mongoose automagically 
-// creates a pluralised collection in the database (e.g. db.blogs).
-// Convention is to capitalize a model ("Blog"), just like with PHP classes...
+// Compile the Blog schema into a Blog model (kind of like a class in PHP) that can be used to create, read, update and delete blogs in the DB.
 var Blog = mongoose.model('Blog', blogSchema);
 
 app.get('/', function(req, res) {
@@ -36,13 +32,6 @@ app.get('/', function(req, res) {
 })
 
 // INDEX restful route
-// After the '/blogs' route is hit (i.e. a get request to '/blogs' is performed by entering 
-// that URL), a callback function is called. It's passed the request and response object (how?)
-// When Blog.find() returns all blogs from the mongoDB:
-// - all the blogs are returned and passed as an object into the callback. So is an error message, if there is one.
-// - the passed callback function is called. The inner callback has access to the response object
-// passed into the outer callback (for res.render) and all the blogs. These are passed into index.ejs
-// as the 'blogs' value on the 'blogs' key (which can be used in index.ejs).
 app.get('/blogs', function(req, res) {
   Blog.find({}, function(err, blogs) {
     if(err) {
@@ -52,6 +41,13 @@ app.get('/blogs', function(req, res) {
     }
   });
 });
+
+// NEW restful route
+app.get('/blogs/new', function(req, res) {
+  res.render('new');
+});
+
+// CREATE ROUTE
 
 
 // Start the server, running on port 3000
