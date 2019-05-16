b8bd23f6c873d9a54c33e298d7c43bf9372fc09a
Kevin O'Brien <kob123@hotmail.co.uk>
2019-05-16 21:05:26 +0100

    RESTful Blog App: NEW and CREATE

--- app.js
+++ app.js
@@ -48,7 +48,17 @@ app.get('/blogs/new', function(req, res) {
 });
 
 // CREATE ROUTE
-
+app.post('/blogs', function(req, res) {
+  // Create blog
+  Blog.create(req.body.blog, function(err, newBlog) {
+    if(err) {
+      res.render('new');
+    } else {
+      // then redirect to the index
+      res.redirect('/blogs');
+    }
+  }) 
+});
 
 // Start the server, running on port 3000
 app.listen(3000, function(req, res) {
